c89844f72e46827f5c975e1f21760f3f7b09bcf3
BaptisteD <96989847+GararasLPB@users.noreply.github.com>
2024-09-24 12:19:39 -0400

    disable eslint any error

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export interface simpleBlogCard {
     title: string;
     smallDescription: string;
--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
